a518b0f09890d482e5a91c38e5e0f6c8241ca034
naseem <naseem.mohammad@fluper.in>
2023-10-22 23:36:37 +0530

    some_question

--- interview.py
+++ interview.py
@@ -44,7 +44,7 @@ def findMissingNumbers(n):
             output.append(i)
     return output
     
-listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16,4]
 print(findMissingNumbers(listOfNumbers))
 
 
--- oneyes.py
+++ oneyes.py
@@ -39,3 +39,83 @@
 #             return -1
         
 # print(Binary([10,20,30,40],40))
+
+#28 Linear search algorithm
+
+# def linear_search(arr, target):
+#     for i in range(len(arr)):
+#         print(i)
+#         if arr[i] == target:
+#             return i
+#         return 1
+
+# print(linear_search([27, 13, 32, 20,40],20))
+
+
+# def findMissingNumbers(n):
+#     output = []
+#     for i in range(1,n[-1]):
+#         if i not in n:
+#             output.append(i)
+#     return output
+
+# listOfNumbers = [3, 5, 50,7,23,16,4,44,24,33,1]
+# sortedlist = sorted(listOfNumbers)
+# print(sortedlist)
+# print("The missing numbers are :",findMissingNumbers(sortedlist))
+
+
+                 
+                 ################### Dictionary Program #############
+
+#1 Find occurence of each character in string
+#2 Find occurence of vowel present in given string
+#3 Check Whether string are anagrams or not
+#4 show output is ax1,by2,cz3,d4,e5,f,g
+#5 Input = ABAABBCA and output = 4A3B1C
+
+# s = 'QWERTYUIOPZXCVBNMPOIUYTREWQZMNBVCXZCVXNYIEIYOETRMB'
+# d = {}
+# for ch in s:
+#     d[ch] = d.get(ch, 0) + 1
+# for k,v in sorted(d.items()):
+#     print('{} occured {} times'.format(k,v))
+
+# s = 'uuuuuiiiooouuaaaaaeeeeeiiiQWERTYUIOPZXCVBNMPOIUYTREWQZMNBVCXZCVXNYIEIYOETRMB'
+# s1 = s.upper()
+# v = {'a','e','i','o,','u','A','E','I','O','U'}
+# d = {}
+# for ch in s1:
+#     if ch in v:
+#         d[ch] = d.get(ch, 0) + 1
+# for k,v in sorted(d.items()):
+#     print('{} occured {} times'.format(k,v))
+
+# Input = ABAABBCA and output = 4A3B1C
+
+# s = 'ABAABBCA'
+# d = {}
+# output = ''
+# for ch in s:
+#     d[ch ]= d.get(ch,0)+1
+# for k,v in d.items():
+#     output = output + k + str(v)
+# print(output)
+
+# show output is ax1,by2,cz3,d4,e5,f,g
+s1 = "abcdefg"
+s2 = "xyz"
+s3 = '12345'
+i=j=k=0
+while i<len(s1) or j<len(s2) or k<len(s3):
+    output = ''
+    if i<len(s1):
+        output = output + s1[i]
+        i += 1
+    if j<len(s2):
+        output = output + s2[j]
+        j += 1
+    if k<len(s3):
+        output = output + s3[k] 
+        k += 1
+    print(output)
