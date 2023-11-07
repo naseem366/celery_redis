beef39ccfb8cff92d35841515bb8b3e00a4b2578
Mohd Naseem <54537980+naseem366@users.noreply.github.com>
2023-11-07 14:32:29 +0530

    Update interview.py

--- interview.py
+++ interview.py
@@ -708,3 +708,16 @@ Start Celery Beat: Run the Celery Beat scheduler using the following command:
 celery -A celeryapp:app beat --loglevel=info
 
 
+def lengthOfLongestSubstring(s):
+        maxLen = 1
+        if s == '':
+            return 0                      # Dealing with one edge case
+        for i in range(len(s)):
+            substring = s[i]              # Initialising the substring
+            for j in range(i+1, len(s)):  # Starting to append characters to substring from i+1
+                if s[j] not in substring: # As long as its not repeating. "not in" can be used to check if the character isn't already there in the substring
+                    substring = substring + s[j]
+                    maxLen = max(maxLen, len(substring)) # Updating maxLen if it is greater than the existing maxLen
+                else:
+                    break
+        return maxLen
