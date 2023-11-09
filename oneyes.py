cee257785bf9f54a1b7d7c6a815d3a36b2b7e3c1
naseem <naseem.mohammad@fluper.in>
2023-11-09 22:24:10 +0530

    star_question

--- oneyes.py
+++ oneyes.py
@@ -103,28 +103,21 @@
 
 
 
-# n=5
-# p=1
-# for i in range(n):
-#     for j in range(i,n):
-#         print(" ",end="")
-        
-#     for j in range(i):
-#         print("#",end="")
-#     for j in range(i+1):
-#         print("$",end="")
-#     p+=1
-#     print() 
-
-
-
 n=5
-p=69
+p=1
 for i in range(n):
-    for j in range(i+1):
-        print(chr(p),end="")
-    p-=1
-    print()
+    for j in range(i,n):
+        print(" ",end="")
+    if i%2==0:
+        for j in range(i):
+            print("#",end="")
+    else:
+        for j in range(i+1):
+            print("$",end="")
+    print() 
+
+
+
 
 
 
--- star.py
+++ star.py
@@ -586,7 +586,19 @@ for i in range(n):
     print()
 
 ############################################ Print this type of pattern ###############
+# A
+# CC
+# EEE
+# GGGG
+# IIIII
 
+n=5
+p=65
+for i in range(n):
+    for j in range(i+1):
+        print(chr(p),end="")
+    p+=2
+    print()
 
 ############################################ Print this type of pattern ###############
 
