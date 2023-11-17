f6d9d34b44627e1552407cb800f7fba37f35ea42
Mohd Naseem <nasemmohammad786@gmail.com>
2023-11-17 21:30:46 +0530

    changes

--- bosscoder_question.py
+++ bosscoder_question.py
@@ -1,6 +1,14 @@
+### Implement a program that finds the largest number in the list 
+lst = [2,4,1,55,6,9]
+max_val = lst[0]
+for val in lst:
+    if val > max_val:  
+        max_val = val
+print ("The maximum value is :", max_val)
+
+
 ### Calculate the compound interest for a given principal amount, interest rate, and time period
 # The formula used is: The formula is : CI = P(1+R/100)^T - P
-
 principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the rate of interest: "))
 time = float(input("Enter the time in years: "))
@@ -17,13 +25,11 @@ remaining_days = remaining_days % 7
 print("{} years {} weeks {} days".format(years, weeks, remaining_days))
 
 
-### Implement a program that finds the largest number in the list 
-lst = [2,4,1,55,6,9]
-max_val = lst[0]
-for val in lst:
-    if val > max_val:  
-        max_val = val
-print ("The maximum value is :", max_val)
+
+# Create a program that takes a sentence as input and counts the number of words in it 
+s = "Hello how are you "
+l = s.split()
+print(f"The total word count is {len(l)}")
 
 
 ## Create a program that takes a year as input and checks if it is a leap year or not)
@@ -37,3 +43,21 @@ def checkLeapYear(year):
 checkLeapYear(year)
 
 
+# Implement a program that swaps the values of two variables
+def swap_values(a, b):
+    temp = a
+    a = b
+    b = temp
+    return a, b
+x = 5
+y = 10
+print(swap_values(x,y))
+
+
+# Given a list of numbers, find the sum and average
+def SumAndAvg(numbers):
+    list_sum = sum(numbers)
+    avg = list_sum / len(numbers)
+    return list_sum, avg
+numbers = [1, 2, 3, 4, 5]
+print(SumAndAvg(numbers))
